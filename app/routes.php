13074d85a6022ef6aadddd5abb23f6412f1f9984
Agata Halas <agata.halas@circlekeurope.com>
2020-09-08 16:52:24 +0200

    Initial commit - skeleton

---
+++ app/routes.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListIconsAction;
+use App\Application\Actions\User\ViewIconAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListIconsAction::class);
+        $group->get('/{id}', ViewIconAction::class);
+    });
+};
