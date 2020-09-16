5bb814b8a8c44c744febb71afbe10d9464c2730e
Dawid Nawrot <dawid.nawrot@circlekeurope.com>
2020-09-16 13:50:12 +0200

    Add number of icons and categories in badges

--- src/Application/Actions/Admin/AdminAction.php
+++ src/Application/Actions/Admin/AdminAction.php
@@ -5,19 +5,28 @@ namespace App\Application\Actions\Admin;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
+use Doctrine\ORM\EntityManager;
 
 class AdminAction
 {
     private $view;
+    private $em;
 
-    public function __construct(Twig $view)
+    public function __construct(Twig $view, EntityManager $em)
     {
         $this->view = $view;
+        $this->em = $em;
     }
 
     public function login(ServerRequestInterface $request, ResponseInterface $response, $args)
     {
-        return $this->view->render($response, 'login.html');
+        $number_of_icons = $this->em->getRepository('App\Entity\Icon')->count([]);
+        $number_of_categories = $this->em->getRepository('App\Entity\Category')->count([]);
+
+        return $this->view->render($response, 'login.html', [
+          'number_of_icons' => $number_of_icons,
+          'number_of_categories' => $number_of_categories,
+        ]);
     }
 
     public function logout(ServerRequestInterface $request, ResponseInterface $response, $args)
