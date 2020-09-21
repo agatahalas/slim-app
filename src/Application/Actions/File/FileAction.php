d4f05e123594c4f25fb41e90d03929a415293a78
Dawid Nawrot <dawid.nawrot@circlekeurope.com>
2020-09-21 12:55:25 +0200

    Add custom color support

--- src/Application/Actions/File/FileAction.php
+++ src/Application/Actions/File/FileAction.php
@@ -19,12 +19,20 @@ class FileAction
 
     public function show(Request $request, Response $response, $args)
     {
+        //dd($request->getQueryParams());
         $icon = $this->em->getRepository('App\Entity\Icon')->findBy(['id' => $args['id']]);
         $icon = reset($icon);
         if (!($icon instanceof Icon)) {
             throw new HttpNotFoundException($request, 'No icon found with id: ' . $args['id']);
         }
         $icon = $icon->getArrayIcon();
+        $params = $request->getQueryParams();
+        if (!empty($params['color'])) {
+            $pattern = '/stroke:#[a-f0-9]{6}/m';
+            $icon['src'] = preg_replace($pattern, 'stroke:#' . $params['color'], $icon['src']);
+            $pattern = '/fill:#[a-f0-9]{6}/m';
+            $icon['src'] = preg_replace($pattern, 'fill:#' . $params['color'], $icon['src']);
+        }
         $response->getBody()->write($icon['src']);
         return $response->withHeader('Content-Type', 'image/svg+xml');
     }
